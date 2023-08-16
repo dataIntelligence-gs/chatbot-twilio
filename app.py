a39280b92f5c19c02b8783896d909a99ea9202ab
unknown <globalsol2023@gmail.com>
2023-08-16 12:11:22 -0300

    fixed some code details, added readme.md file

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-from twilio.twiml.messaging_response import MessagingResponse
+from twilio.base.exceptions import TwilioRestException
 from flask import Flask, request
 from twilio.rest import Client
 from dotenv import load_dotenv
-from datetime import datetime
 from waitress import serve
-import requests
-import os
 import datetime as dt
-import ast 
+import os
 import time
 import random
-import os
 
 
 load_dotenv()
@@ -26,131 +22,141 @@ eleccion = None
 
 @app.route('/bot', methods=['POST'])
 def bot():
-    global eleccion
-    to = request.values.get('To')
-    incoming_msg = request.values.get('Body').lower()
-    person = request.values.get('From').lower()
-    number = person.split(':+549')[1]
-    name = request.values.get('ProfileName').lower()
-    now_time = dt.datetime.now()
+    try:
+        global eleccion
+        to = request.values.get('To')
+        incoming_msg = request.values.get('Body').lower()
+        person = request.values.get('From').lower()
+        number = person.split(':+549')[1]
+        name = request.values.get('ProfileName').lower()
+        now_time = dt.datetime.now()
 
-    pool_sales_mendoza = ['2604406536']
-    pool_sales_cordoba = ['3547500771']
-    pool_sales_general = ['2604406536', '3547500771']
-    
-    if incoming_msg == 'en otro momento':
-        account_sid = 'ACce7b9301a1718047284a251f66781145'
-        auth_token = '9eea91c9d052bfbc81cc3a1a672186ae'
-        client = Client(account_sid, auth_token)
-        client.messages.create(
-                            from_=to,
-                            body='Desde EVI desarrollos agradecemos tu tiempo. Te compartimos nuestro sitio web, cualquier consulta no dudes en escribirnos. \n\n*EVI DESARROLLOS*',
-                            to='whatsapp:+54'+number
-                        )
-        time.sleep(random.randint(10, 15))
-
-    elif incoming_msg == "sí, me encantaría":
-        account_sid = 'ACce7b9301a1718047284a251f66781145'
-        auth_token = '9eea91c9d052bfbc81cc3a1a672186ae'
-        client = Client(account_sid, auth_token)
-        client.messages.create(
-                            from_=to,
-                            body="*QUE BUENO QUE SIGAMOS CONVERSANDO* 😁 \n\nTodos nuestros proyectos cuentan con: \n\n- Financiación propia. \n- Cuotas fijas. \n\nHoy tenemos para ofrecerte dos opciones irresistibles: \n\n- *Córdoba* donde descubrirás la belleza natural propia de las sierras 🏞️ \n- *Mendoza* donde te sumergirás en la magia de los Andes y sus viñedos 🏔️ \n\n¿Sobre qué lugar te gustaría recibir más información?",
-                            to='whatsapp:+54'+number
-        )
-
-    elif incoming_msg == "córdoba" or incoming_msg == "mendoza" or incoming_msg == "me interesan ambos":
-        account_sid = 'ACce7b9301a1718047284a251f66781145'
-        auth_token = '9eea91c9d052bfbc81cc3a1a672186ae'
-        client = Client(account_sid, auth_token)
-        client.messages.create(
-                            from_=to,
-                            body="Excelente elección, antes de derivarte con un especialista, te voy a hacer una última pregunta. \n\n¿Qué opción te interesa?",
-                            to='whatsapp:+54'+number
-        )
-        eleccion = incoming_msg
-    
-    elif incoming_msg == "uso turístico" or incoming_msg == "uso residencial" or incoming_msg == "inversión":
-        account_sid = 'ACce7b9301a1718047284a251f66781145'
-        auth_token = '9eea91c9d052bfbc81cc3a1a672186ae'
-        client = Client(account_sid, auth_token)
+        pool_sales_mendoza = ['2942693075']
+        pool_sales_cordoba = ['2942693075']
+        pool_sales_general = ['2942693075']
         
-        if now_time.hour >= 9 and now_time.hour <= 17:
-            if eleccion == "mendoza":
-                a = random.choices(pool_sales_mendoza)
-                client.messages.create(
-                            from_=to,
-                            body=f"¡Hola! El número {number} de {name} eligió la opción {incoming_msg} de {eleccion}, por lo que desea más información. Por favor, contactar a la brevedad, presionando el siguiente link https://wa.me/{number}. ¡Saludos!",
-                            to='whatsapp:+54'+a[0]
-                        )
-
-                client.messages.create(
-                                    from_=to,
-                                    body="¡Genial! 😁 Un asesor de EVI Desarrollos se pondrá en contacto con vos muy pronto para darte toda la información que necesitas para dar el siguiente paso hacia tu nuevo lugar en el mundo. \n\nMientras aguardas la comunicación te comparto nuestro sitio web.",
-                                    to='whatsapp:+54'+number
-                )
-        
-            elif eleccion == "córdoba":
-                a = random.choices(pool_sales_cordoba)
-                client.messages.create(
-                            from_=to,
-                            body=f"¡Hola! El número {number} de {name} eligió la opción {incoming_msg} de {eleccion}, por lo que desea más información. Por favor, contactar a la brevedad, presionando el siguiente link https://wa.me/{number}. ¡Saludos!",
-                            to='whatsapp:+54'+a[0]
-                        )
-
-                client.messages.create(
-                                    from_=to,
-                                    body="¡Genial! 😁 Un asesor de EVI Desarrollos se pondrá en contacto con vos muy pronto para darte toda la información que necesitas para dar el siguiente paso hacia tu nuevo lugar en el mundo. \n\nMientras aguardas la comunicación te comparto nuestro sitio web.",
-                                    to='whatsapp:+54'+number
-                )
-            
-            elif eleccion == "me interesan ambos":
-                a = random.choices(pool_sales_general)
-                client.messages.create(
-                            from_=to,
-                            body=f"¡Hola! El número {number} de {name} eligió la opción {incoming_msg} de {eleccion}, por lo que desea más información. Por favor, contactar a la brevedad, presionando el siguiente link https://wa.me/{number}. ¡Saludos!",
-                            to='whatsapp:+54'+a[0]
-                        )
-
-                client.messages.create(
-                                    from_=to,
-                                    body="¡Genial! 😁 Un asesor de EVI Desarrollos se pondrá en contacto con vos muy pronto para darte toda la información que necesitas para dar el siguiente paso hacia tu nuevo lugar en el mundo. \n\nMientras aguardas la comunicación te comparto nuestro sitio web.",
-                                    to='whatsapp:+54'+number
-                )
-        else:
+        if incoming_msg == 'en otro momento':
+            account_sid = 'ACce7b9301a1718047284a251f66781145'
+            auth_token = '9eea91c9d052bfbc81cc3a1a672186ae'
+            client = Client(account_sid, auth_token)
             client.messages.create(
-                            from_=to,
-                            body="¡Disculpa! Todos nuestros asesores se encuentran fuera de horario laboral. Mañana durante el transcurso de la mañana serás contactado. ¡Muchas gracias por tu tiempo!",
-                            to='whatsapp:+54'+number
-                        )
-            
-            with open(f'offline.txt', 'a', encoding='utf-8') as file:
-                file.write(f'Número: {str(number)}, Nombre: {str(name)}\n')
+                                from_=to,
+                                body='Desde EVI desarrollos agradecemos tu tiempo. Te compartimos nuestro sitio web, cualquier consulta no dudes en escribirnos. \n\n*EVI DESARROLLOS*',
+                                to='whatsapp:+54'+number
+                            )
+            time.sleep(random.randint(10, 15))
+
+        elif incoming_msg == "si, me encantaria":
+            account_sid = 'ACce7b9301a1718047284a251f66781145'
+            auth_token = '9eea91c9d052bfbc81cc3a1a672186ae'
+            client = Client(account_sid, auth_token)
+            client.messages.create(
+                                from_=to,
+                                body="*QUE BUENO QUE SIGAMOS CONVERSANDO* 😁 \n\nTodos nuestros proyectos cuentan con: \n\n- Financiación propia. \n- Cuotas fijas. \n\nHoy tenemos para ofrecerte dos opciones irresistibles: \n\n- *Córdoba* donde descubrirás la belleza natural propia de las sierras 🏞️ \n- *Mendoza* donde te sumergirás en la magia de los Andes y sus viñedos 🏔️ \n\n¿Sobre qué lugar te gustaría recibir más información?",
+                                to='whatsapp:+54'+number
+            )
+
+        elif incoming_msg == "córdoba" or incoming_msg == "mendoza" or incoming_msg == "me interesan ambos":
+            account_sid = 'ACce7b9301a1718047284a251f66781145'
+            auth_token = '9eea91c9d052bfbc81cc3a1a672186ae'
+            client = Client(account_sid, auth_token)
+            client.messages.create(
+                                from_=to,
+                                body="Excelente elección, antes de derivarte con un especialista, te voy a hacer una última pregunta. \n\n¿Qué opción te interesa?",
+                                to='whatsapp:+54'+number
+            )
+            eleccion = incoming_msg
         
-    elif incoming_msg == "ir con asistente":
-        a = random.choices(pool_sales_general)
-        client.messages.create(
-                    from_=to,
-                    body=f"¡Hola! El número {number} de {name} eligió la opción {incoming_msg} de {eleccion}, por lo que desea más información. Por favor, contactar a la brevedad, presionando el siguiente link https://wa.me/{number}. ¡Saludos!",
-                    to='whatsapp:+54'+a[0]
-                )
+        elif incoming_msg == "uso turístico" or incoming_msg == "uso residencial" or incoming_msg == "inversión":
+            account_sid = 'ACce7b9301a1718047284a251f66781145'
+            auth_token = '9eea91c9d052bfbc81cc3a1a672186ae'
+            client = Client(account_sid, auth_token)
+            
+            if now_time.hour >= 9 and now_time.hour <= 17:
+                if eleccion == "mendoza":
+                    a = random.choices(pool_sales_mendoza)
+                    client.messages.create(
+                                from_=to,
+                                body=f"¡Hola! El número {number} de {name} eligió la opción {incoming_msg} de {eleccion}, por lo que desea más información. Por favor, contactar a la brevedad, presionando el siguiente link https://wa.me/{number}. ¡Saludos!",
+                                to='whatsapp:+54'+a[0]
+                            )
 
-    else:
-        account_sid = 'ACce7b9301a1718047284a251f66781145'
-        auth_token = '9eea91c9d052bfbc81cc3a1a672186ae'
-        client = Client(account_sid, auth_token)
-        client.messages.create(
-                            from_=to,
-                            body="El mensaje no pudo ser interpretado por nuestro asistente virtual. Te pido por favor que selecciones una de las alternativas disponibles o ingreses *IR CON ASISTENTE* para que uno de nuestros agentes pueda colaborarte en lo que necesites. Muchas gracias por tu tiempo.",
-                            to='whatsapp:+54'+number
-                        )
-    
-    print(incoming_msg, person, name)
+                    client.messages.create(
+                                        from_=to,
+                                        body="¡Genial! 😁 Un asesor de EVI Desarrollos se pondrá en contacto con vos muy pronto para darte toda la información que necesitas para dar el siguiente paso hacia tu nuevo lugar en el mundo. \n\nMientras aguardas la comunicación te comparto nuestro sitio web.",
+                                        to='whatsapp:+54'+number
+                    )
+            
+                elif eleccion == "córdoba":
+                    a = random.choices(pool_sales_cordoba)
+                    client.messages.create(
+                                from_=to,
+                                body=f"¡Hola! El número {number} de {name} eligió la opción {incoming_msg} de {eleccion}, por lo que desea más información. Por favor, contactar a la brevedad, presionando el siguiente link https://wa.me/{number}. ¡Saludos!",
+                                to='whatsapp:+54'+a[0]
+                            )
 
-    with open(f"conversations_interesed/{str(number)}.txt", "a", encoding='utf-8') as f:
-        f.write(f"{str(name)}: {str(incoming_msg)} - {str(now_time)}"+'\n')
+                    client.messages.create(
+                                        from_=to,
+                                        body="¡Genial! 😁 Un asesor de EVI Desarrollos se pondrá en contacto con vos muy pronto para darte toda la información que necesitas para dar el siguiente paso hacia tu nuevo lugar en el mundo. \n\nMientras aguardas la comunicación te comparto nuestro sitio web.",
+                                        to='whatsapp:+54'+number
+                    )
+                
+                elif eleccion == "me interesan ambos":
+                    a = random.choices(pool_sales_general)
+                    client.messages.create(
+                                from_=to,
+                                body=f"¡Hola! El número {number} de {name} eligió la opción {incoming_msg} de {eleccion}, por lo que desea más información. Por favor, contactar a la brevedad, presionando el siguiente link https://wa.me/{number}. ¡Saludos!",
+                                to='whatsapp:+54'+a[0]
+                            )
 
-    return str('Done')
+                    client.messages.create(
+                                        from_=to,
+                                        body="¡Genial! 😁 Un asesor de EVI Desarrollos se pondrá en contacto con vos muy pronto para darte toda la información que necesitas para dar el siguiente paso hacia tu nuevo lugar en el mundo. \n\nMientras aguardas la comunicación te comparto nuestro sitio web.",
+                                        to='whatsapp:+54'+number
+                    )
+            else:
+                client.messages.create(
+                                from_=to,
+                                body="¡Disculpa! Todos nuestros asesores se encuentran fuera de horario laboral. Mañana durante el transcurso de la mañana serás contactado. ¡Muchas gracias por tu tiempo!",
+                                to='whatsapp:+54'+number
+                            )
+                
+                with open(f'offline.txt', 'a', encoding='utf-8') as file:
+                    file.write(f'Número: {str(number)}, Nombre: {str(name)}\n')
+            
+        elif incoming_msg == "ir con asistente":
+            a = random.choices(pool_sales_general)
+            client.messages.create(
+                        from_=to,
+                        body=f"¡Hola! El número {number} de {name} eligió la opción {incoming_msg} de {eleccion}, por lo que desea más información. Por favor, contactar a la brevedad, presionando el siguiente link https://wa.me/{number}. ¡Saludos!",
+                        to='whatsapp:+54'+a[0]
+                    )
+
+        else:
+            account_sid = os.environ['TWILIO_ACCOUNT_SID']
+            auth_token = os.environ['TWILIO_AUTH_TOKEN']
+            client = Client(account_sid, auth_token)
+            client.messages.create(
+                                from_=to,
+                                body="El mensaje no pudo ser interpretado por nuestro asistente virtual. Te pido por favor que selecciones una de las alternativas disponibles o ingreses *IR CON ASISTENTE* para que uno de nuestros agentes pueda colaborarte en lo que necesites. Muchas gracias por tu tiempo.",
+                                to='whatsapp:+54'+number
+                            )
+        
+        print(incoming_msg, person, name)
+
+        with open(f"conversations_interesed/{str(number)}.txt", "a", encoding='utf-8') as f:
+            f.write(f"{str(name)}: {str(incoming_msg)} - {str(now_time)}"+'\n')
+
+        return str('Done')
+    except TwilioRestException as twilio_error:
+        if twilio_error.status == 404 and 'No valid template found' in e.msg:
+            print(f'Plantilla dada de baja - detalles:{e}')
+            
+        else:
+            print(f'Error al momento de enviar mensaje - Detalles: TwilioRestException - {e}')
+
+    except Exception as e:
+        print(f'Error general en la funcion bot - Detalles: {e}')
 
 @app.route('/send/campaign1', methods=['POST'])
 def send():
@@ -173,8 +179,17 @@ def send():
             time.sleep(random.randint(10, 15))
 
             print(message.sid)
+        
+        except TwilioRestException as twilio_error:
+            if twilio_error.status == 404 and 'No valid template found' in e.msg:
+                print(f'Plantilla de saludo de bot dada de baja - detalles:{e}')
+                continue
+            else:
+                print(f'Error al momento de enviar mensaje - Detalles: TwilioRestException - {e}')
+                continue
+
         except Exception as e:
-            print(f'Ocurrio un error en la funcion send - Detalles: {e}')
+            print(f'Error general en la funcion send - Detalles: {e}')
             continue
         
     return str('Done')
